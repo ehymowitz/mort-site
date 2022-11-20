0aac82a459930fd319d7ed4c33cec93aa2b0a28e
Elie Hymowitz <ehymowitz@gmail.com>
2022-11-20 13:02:24 +1100

    feat: preliminary color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    theme: {
+      colors: {
+        transparent: "transparent",
+        dark: "#040406",
+        lightBlue: "#ABCCFF",
+        lightTan: "#D3B4A2",
+        accent: "#925707",
+      },
+    },
     extend: {},
   },
   plugins: [],
